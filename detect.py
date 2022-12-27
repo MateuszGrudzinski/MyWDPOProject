3a4eca63d18e028a925cc516a00ea22bd3e5200e
Mateusz Grudziński <77492508+MateuszGrudzinski@users.noreply.github.com>
2022-12-27 18:00:36 +0100

    Wyswietlanie zdjec

--- detect.py
+++ detect.py
@@ -7,6 +7,8 @@ import click
 import cv2
 from tqdm import tqdm
 
+cv2.namedWindow('temp',cv2.WINDOW_NORMAL)
+
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
 
@@ -20,12 +22,12 @@ def detect(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    cv2.namedWindow('temp',cv2.WINDOW_AUTOSIZE)
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    cv2.imshow('temp',img)
-    time.sleep(5)
     #TODO: Implement detection method.
-    
+
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    cv2.imshow('temp', img)
+    cv2.waitKey(0)
+
     red = 0
     yellow = 0
     green = 0
@@ -41,7 +43,6 @@ def main(data_path: Path, output_file_path: Path):
 
     temp_path1 = Path(data_path.decode('utf-8'))
     img_list = temp_path1.glob('*.jpg')
-
     results = {}
 
     for img_path in tqdm(sorted(img_list)):

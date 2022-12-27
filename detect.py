aef9e866eadd844591eaa975fbb0aa41ee2c0ef5
Mateusz Grudziński <77492508+MateuszGrudzinski@users.noreply.github.com>
2022-12-27 21:14:16 +0100

    Zliczanie zółtych cukierków _ work in progress

--- detect.py
+++ detect.py
@@ -2,12 +2,11 @@ import glob
 import json
 from pathlib import Path
 from typing import Dict
-import time
 import click
 import cv2
+import numpy as np
 from tqdm import tqdm
 
-cv2.namedWindow('temp',cv2.WINDOW_NORMAL)
 
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
@@ -24,8 +23,61 @@ def detect(img_path: str) -> Dict[str, int]:
     """
     #TODO: Implement detection method.
 
+
+    cv2.namedWindow('org', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('threshold_y', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('canny_y', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('threshold_g', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('threshold_p', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('threshold_r', cv2.WINDOW_NORMAL)
+
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    cv2.imshow('temp', img)
+    blur = cv2.GaussianBlur(img,(7,7),0)
+    img_hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    minhsv_y = np.array([20, 100, 100])
+    maxhsv_y = np.array([30, 255, 255])
+    maskhsv = cv2.inRange(img_hsv, minhsv_y, maxhsv_y)
+    resulthsv = cv2.bitwise_and(img_hsv, img_hsv, mask=maskhsv)
+    yellow_img_raw = cv2.cvtColor(resulthsv, cv2.COLOR_HSV2BGR)
+    yellow_img = cv2.medianBlur(yellow_img_raw,5)
+    yellow_canny = cv2.Canny(yellow_img, 10, 150, 3)
+    dilated_y = cv2.dilate(yellow_canny, (1, 1), iterations=0)
+
+    (cnt, hierarchy) = cv2.findContours(
+        dilated_y.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    print(len(cnt))
+
+    minhsv_g = np.array([35, 30, 30])
+    maxhsv_g = np.array([65, 255, 255])
+    maskhsv = cv2.inRange(img_hsv, minhsv_g, maxhsv_g)
+    resulthsv = cv2.bitwise_and(img_hsv, img_hsv, mask=maskhsv)
+    green_img = cv2.cvtColor(resulthsv, cv2.COLOR_HSV2BGR)
+
+    minhsv_p = np.array([150, 20, 20])
+    maxhsv_p = np.array([170, 255, 255])
+    maskhsv = cv2.inRange(img_hsv, minhsv_p, maxhsv_p)
+    resulthsv = cv2.bitwise_and(img_hsv, img_hsv, mask=maskhsv)
+    purple_img = cv2.cvtColor(resulthsv, cv2.COLOR_HSV2BGR)
+
+    minhsv_r = np.array([176, 100, 100])
+    maxhsv_r = np.array([179, 200, 200])
+    maskhsv1 = cv2.inRange(img_hsv, minhsv_r, maxhsv_r)
+    minhsv_r = np.array([0, 50, 50])
+    maxhsv_r = np.array([10, 255, 255])
+    maskhsv2 = cv2.inRange(img_hsv, minhsv_r, maxhsv_r)
+    maskhsv = cv2.bitwise_or(maskhsv1, maskhsv2)
+    resulthsv = cv2.bitwise_and(img_hsv, img_hsv, mask=maskhsv)
+    red_img = cv2.cvtColor(resulthsv, cv2.COLOR_HSV2BGR)
+
+    cv2.imshow('org', img)
+    cv2.imshow('threshold_y', yellow_img)
+    cv2.imshow('canny_y', yellow_canny)
+    cv2.imshow('threshold_g', green_img)
+    cv2.imshow('threshold_p', purple_img)
+    cv2.imshow('threshold_r', red_img)
+
     cv2.waitKey(0)
 
     red = 0
